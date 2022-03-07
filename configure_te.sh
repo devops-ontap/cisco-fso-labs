0eaa85ed719b996f02e70d41c8c842878c5ba691
sconrod-tester <sconrod1@gmail.com>
2022-03-07 16:59:54 -0700

    updates

--- configure_te.sh
+++ configure_te.sh
@@ -5,6 +5,7 @@ export AWS_PAGER=""
 rm -rf __pycache__
 pip3 install paramiko
 apt -y install ncurses-term
+apt -y install scp
 ssh-keyscan -H 3.145.46.2 >> ~/.ssh/known_hosts
 python3 configure_te.py
 
