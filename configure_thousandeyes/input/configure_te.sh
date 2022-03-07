f40ac49bc0124dd0ddc080df270db90a4e47f7ce
sconrod-devops <sconrod1@gmail.com>
2022-03-07 16:19:59 -0700

    Us east 2a (#8)
    * updates
    
    * updates

---
+++ configure_thousandeyes/input/configure_te.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd input
+chmod 400 *.pem
+export AWS_PAGER=""
+rm -rf __pycache__
+pip3 install paramiko
+apt -y install ncurses-term
+ssh-keyscan -H 3.145.46.2 >> ~/.ssh/known_hosts
+python3 configure_te.py
+
