a62975c49de8d294fed03ee0575e3243ab51a6f9
sconrod-tester <sconrod1@gmail.com>
2022-03-07 09:32:37 -0700

    updates

---
+++ configure_thousandeyes/input/configure_te.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import json, re, sys, os, json, subprocess
+from subprocess import call, check_output
+#Download the TE Agent
+#Scp the Agent to the Ubuntu
+#Install the Agent
+
+key_file='us-east-2a.pem'
+username='ubuntu'
+ip='UPDATE THE IP OF CSR HERE'
+
+
+
+
