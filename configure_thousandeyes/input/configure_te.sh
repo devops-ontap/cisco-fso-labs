67933fac2e9cdd6e24302a488a78227718f982fd
sconrod-tester <sconrod1@gmail.com>
2022-03-07 17:05:32 -0700

    updates

--- configure_thousandeyes/input/configure_te.sh
+++ configure_thousandeyes/input/configure_te.sh
@@ -5,6 +5,9 @@ export AWS_PAGER=""
 rm -rf __pycache__
 pip3 install paramiko
 apt -y install ncurses-term
+/usr/local/bin/python -m pip install --upgrade pip
+curl -Os https://downloads.thousandeyes.com/agent/install_thousandeyes.sh
 ssh-keyscan -H 3.145.46.2 >> ~/.ssh/known_hosts
+scp -i us-east-2a.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null install_thousandeyes.sh ubuntu@3.145.46.2:/tmp
 python3 configure_te.py
 
