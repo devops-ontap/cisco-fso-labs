a5bb9acd6f640551f1f2f0ae6413b0d489e0ee52
sconrod-tester <sconrod1@gmail.com>
2022-03-18 15:47:06 -0600

    updates

--- thousandeyes/te_config/input/configure_te_agents.sh
+++ thousandeyes/te_config/input/configure_te_agents.sh
@@ -8,10 +8,6 @@ pip3 install paramiko
 pip3 install requests
 pip3 install urllib3
 apt -y install ncurses-term
-#must to a for each here to copy the install file to each instance...
-
-#ssh-keyscan -H localhost >> ~/.ssh/known_hosts
-#scp -i sshkey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null install_thousandeyes.sh ubuntu@IP:/tmp
 python3 configure_te_agents.py
 
 
