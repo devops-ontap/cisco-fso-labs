bb81100e36c82f659fad5140a4c22bd0033d9c72
sconrod-tester <sconrod1@gmail.com>
2022-03-25 18:27:50 -0600

    updates

--- aws_key_vault.sh
+++ aws_key_vault.sh
@@ -19,11 +19,12 @@ touch ssh-key.json
 awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $PRIVATE_KEY > ssh-key.json
 #Later iteration, set up access so that the key can be written to vault for the team, for now manually add it.
 #This is where send the key to the vault under the team name
-export VAULT_ADDR=http://vault.devops-ontap.com
+export VAULT_ADDR=http://vault.devops-ontap.com:8200
 #get the vault address from the vault via var
 #How to logon to vault with the cli to do this....
-export VAULT_TOKEN=$VSSH_TOKEN
+export VAULT_TOKEN=$SSH_TOKEN
 #vault kv put kv-v1/prod/cert/mysql cert=@cert.pem
+vault login $SSH_TOKEN
 vault kv put concourse/cisco-fso-labs/ssh-cert cert=@ssh-key.pem
 
 
