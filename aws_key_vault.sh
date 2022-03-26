b093d7483741040d7f58b9d0e6cb6ae7eaa2bff3
sconrod-tester <sconrod1@gmail.com>
2022-03-25 18:48:38 -0600

    updates

--- aws_key_vault.sh
+++ aws_key_vault.sh
@@ -5,7 +5,7 @@ apt -y update
 apt -y install jq
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && sudo apt-get install vault
+apt-get update && apt-get install vault
 setcap cap_ipc_lock= /usr/bin/vault
 apt -y install jq
 python3 aws_key.py
