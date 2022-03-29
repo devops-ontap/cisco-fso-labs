3395cca971c31d814a46cb3e70dce6b0d47a7772
sconrod-tester <sconrod1@gmail.com>
2022-03-29 15:09:44 -0600

    updates

--- configure_te_agents.sh
+++ configure_te_agents.sh
@@ -3,13 +3,9 @@ export AWS_PAGER=""
 pip3 install paramiko
 #apt -y install ncurses-term
 #Call the vault and set the SSH key to env var
-
-#See if it can read in the key value
-echo $PRIVATE_KEY
-
 #export VAULT_ADDR=$VAULT_ADDR
-#vault login --no-print $SSH_TOKEN
-#vault kv get concourse/cisco-fso-labs/$NAME ssh-key=@$PRIVATE_KEY
-python3 configure_te_agents.py
+vault login --no-print $SSH_TOKEN
+vault kv get concourse/cisco-fso-labs/$NAME ssh-key
+#python3 configure_te_agents.py
 
 
