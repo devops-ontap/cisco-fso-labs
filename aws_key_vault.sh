8eccb7a4a2f0d1f4d4762f18337b855278404a97
sconrod-tester <sconrod1@gmail.com>
2022-03-29 10:40:09 -0600

    updates

--- aws_key_vault.sh
+++ aws_key_vault.sh
@@ -2,21 +2,22 @@
 export AWS_PAGER=""
 rm -rf __pycache__
 apt -y update
-apt -y install jq
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && apt-get install vault
+#apt -y install jq
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+#apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#apt-get update && apt-get install vault
 setcap cap_ipc_lock= /usr/bin/vault
-apt -y install jq
+#apt -y install jq
 python3 aws_key.py
 ls -la *.pem
 cat *.pem
-cp *.pem ssh-key.pem
-PRIVATE_KEY='ssh-key.pem'
+cp *.pem $NAME-ssh-key.pem
+PRIVATE_KEY=$NAME+'-ssh-key.pem'
+echo "checking that the PRIVATE_KEY is resolving to the Name VAR plus -ssh-key.pem"
 #echo "echo-ing out the var $PRIVATE_KEY
 echo $PRIVATE_KEY
 touch ssh-key.json
-awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $PRIVATE_KEY > ssh-key.json
+awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $PRIVATE_KEY > $PRIVATE_KEY.json
 #Later iteration, set up access so that the key can be written to vault for the team, for now manually add it.
 #This is where send the key to the vault under the team name
 export VAULT_ADDR=http://vault.devops-ontap.com:8200
@@ -25,7 +26,7 @@ export VAULT_ADDR=http://vault.devops-ontap.com:8200
 echo $SSH_TOKEN
 #vault kv put kv-v1/prod/cert/mysql cert=@cert.pem
 vault login $SSH_TOKEN
-vault kv put concourse/cisco-fso-labs/ssh-cert cert=@ssh-key.pem
+vault kv put concourse/cisco-fso-labs/$NAME-ssh-cert cert=@ssh-key.pem
 
 
 
