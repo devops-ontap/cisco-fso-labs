0edafcee16240e6196eb13a76f5da1b722a06c6e
sconrod-tester <sconrod1@gmail.com>
2022-03-29 16:55:53 -0600

    updates

--- aws_key_vault.sh
+++ aws_key_vault.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-#printenv
 export AWS_PAGER=""
 #This is required for vault
 setcap cap_ipc_lock= /usr/bin/vault
+cd git-resource
 python3 aws_key.py
-cp *.pem ssh-key.pem
-#This may be deprecated in the future
-touch touch $PRIVATE_KEY.json
-awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $PRIVATE_KEY >  $PRIVATE_KEY.json
-#Consider having each Lab User Setup with their own Team in CI and Vault
+cat *.pem
+PRIVATE_KEY=$(ls *.pem)
+echo $PRIVATE_KEY
+#touch touch $PRIVATE_KEY.json
+#awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $PRIVATE_KEY >  $PRIVATE_KEY.json
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $SSH_TOKEN
-vault kv put concourse/cisco-fso-labs/$NAME ssh-key=@ssh-key.pem
+vault kv put concourse/cisco-fso-labs/$NAME ssh-key=@$PRIVATE_KEY
 
 
 
