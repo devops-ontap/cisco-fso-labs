f3cbd9ce32d7ee9ae09dfc0af1a91b282b5fc2a8
sconrod-tester <sconrod1@gmail.com>
2022-03-29 16:05:18 -0600

    updates

--- aws_key_vault.sh
+++ aws_key_vault.sh
@@ -4,18 +4,15 @@ export AWS_PAGER=""
 #This is required for vault
 setcap cap_ipc_lock= /usr/bin/vault
 python3 aws_key.py
-PRIVATE_KEY=$(ls *.pem)
-cat $PRIVATE_KEY
+cp *.pem ssh-key.pem
 #This may be deprecated in the future
 touch touch $PRIVATE_KEY.json
 awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $PRIVATE_KEY >  $PRIVATE_KEY.json
 #Consider having each Lab User Setup with their own Team in CI and Vault
 export VAULT_ADDR=$VAULT_ADDR
-export VAULT_TOKEN=$SSH_TOKEN
-#Sample Command to logon to vault with the cli to do this....
-#vault kv put kv-v1/prod/cert/mysql cert=@cert.pem
+export VAULT_TOKEN=$SSH_TOKE
 vault login --no-print $SSH_TOKEN
-vault kv put concourse/cisco-fso-labs/$NAME/aws-ssh ssh-key=@$PRIVATE_KEY
+vault kv put concourse/cisco-fso-labs/$NAME/aws-ssh ssh-key=@ssh-key.pem
 
 
 
