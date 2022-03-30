68cadf6a051ee7db125a2ea8a8a81176fb64e0f2
sconrod-tester <sconrod1@gmail.com>
2022-03-30 14:01:56 -0600

    updates

--- configure_te_agents.sh
+++ configure_te_agents.sh
@@ -19,7 +19,7 @@ echo "${SSHKEY}" | ssh-add -
 for server in $(cat hostfile)
 do
   ssh-keyscan -H "$server" >> ~/.ssh/known_hosts
-  ssh -i sshkey.pem ubuntu@'$server' env TE_GROUP=$TE_GROUP
+  ssh -i sshkey.pem ubuntu@"$server" env TE_GROUP=$TE_GROUP
 done
 #Need to figure out what is required for the Instance to be able to set the env var or call vault to pass in the token
 #remotely set the VAR for now
