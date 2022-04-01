333ecd37aa5958600ef518b716680af545cc5fe1
sconrod-tester <sconrod1@gmail.com>
2022-04-01 15:25:25 -0600

    updates

--- configure_te_agents.sh
+++ configure_te_agents.sh
@@ -2,10 +2,8 @@
 export AWS_PAGER=""
 pip3 install paramiko
 #apt -y install ncurses-term
-#Call the vault and set the SSH key to env var
 export VAULT_ADDR=$VAULT_ADDR
 export SSH_TOKEN=$SSH_TOKEN
-export TE_GROUP=$TE_GROUP
 vault login --no-print $SSH_TOKEN
 vault kv get --field=ssh-key concourse/cisco-fso-labs/$NAME >> sshkey.pem
 SSHKEY='sshkey.pem'
@@ -13,14 +11,9 @@ chmod 400 sshkey.pem
 mkdir ~/.ssh
 touch ~/.ssh/known_hosts
 echo "${SSHKEY}" | ssh-add -
-touch var
-echo $TE_GROUP > var
 for server in $(cat hostfile)
 do
   ssh-keyscan -H "$server" >> ~/.ssh/known_hosts
-# ssh -i sshkey.pem ubuntu@"$server" env TE_GROUP=$TE_GROUP
-  scp -i sshkey.pem var ubuntu@"$server":~/
-  scp -i sshkey.pem var.sh ubuntu@"$server":~/
 done
 python3 configure_te_agents.py
 
