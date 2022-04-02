7bfd3412216acc2f9ba7f45e6b5991d28154630c
sconrod-tester <sconrod1@gmail.com>
2022-04-01 18:26:02 -0600

    updates

--- set_var.sh
+++ set_var.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch .bash_profile
-export VAULT_ADDR=http://vault.devops-ontap.com:8200
-echo $VAULT_ADDR
-env VAULT_ADDR=http://vault.devops-ontap.com:8200
+echo "#!/bin/bash" >> /env.sh
+echo "export VAULT_ADDR=http://vault.devops-ontap.com:8200" >> /env.sh
+chmod +x /env.sh
+/env.sh
