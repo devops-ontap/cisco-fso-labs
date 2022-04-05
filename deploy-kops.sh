6df976e847792f54111b6351be20644da8b7689d
sconrod-tester <sconrod1@gmail.com>
2022-04-04 23:42:05 -0600

    updates

--- deploy-kops.sh
+++ deploy-kops.sh
@@ -3,13 +3,13 @@ export AWS_PAGER=""
 #Install Kops & kubectl
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+mv ./kubectl /usr/local/bin/kubectl
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+apt-get update
+apt-get install -y kubectl
 kubectl --version
 
 #Prepare LAB IAM for Kops & Add  the AWS IAM Creds to Vault
