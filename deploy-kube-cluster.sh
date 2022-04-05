cb156ca8f7f9fd6f3697da401a1182036f5858e7
sconrod-tester <sconrod1@gmail.com>
2022-04-05 17:24:33 -0600

    updates

--- deploy-kube-cluster.sh
+++ deploy-kube-cluster.sh
@@ -2,7 +2,8 @@
 export AWS_PAGER=""
 export VAULT_ADDR=$VAULT_ADDR
 export SSH_TOKEN=$SSH_TOKEN
-export TE_GROUP=$TE_GROUP
-export AWS_KEY_ID=((Access_key_ID.Access_key))
-export AWS_KEY=((Secret_access_key.Secret_access_key))
-print $AWS_KEY_ID
+vault login --no-print $SSH_TOKEN
+AWS_KEY_ID=$(vault kv get --field=AccessKeyId concourse/main/lab-kops)
+AWS_SECRET=$(vault kv get --field=SecretAccessKey concourse/main/lab-kops)
+echo $AWS_KEY_ID
+echo $AWS_SECRET
