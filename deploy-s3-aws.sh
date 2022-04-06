1889ac6804aa924ea2d21166a77d50128e02251f
sconrod-tester <sconrod1@gmail.com>
2022-04-06 10:22:29 -0600

    updates

--- deploy-s3-aws.sh
+++ deploy-s3-aws.sh
@@ -3,7 +3,7 @@ export AWS_PAGER=""
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $SSH_TOKEN
-export AZ=$AZ
+export AZ=$NAME
 export REGION=$REGION
 
 
@@ -14,7 +14,7 @@ aws s3api create-bucket --bucket lab-kops -oidc-store --region $REGION --acl pub
 Create First Cluster:
 export CLUSTER_NAME=lab-kube.k8s.local
 export KOPS_STATE_STORE=s3://lab-kube.k8s.local
-kops create cluster --zones=$AZ ${CLUSTER_NAME}
+kops create cluster --zones=$NAME ${CLUSTER_NAME}
 kops update cluster ${CLUSTER_NAME} --yes
 
 
