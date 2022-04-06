574c1bcecf248de10ae21499f81366eb3d087667
sconrod-tester <sconrod1@gmail.com>
2022-04-06 13:05:59 -0600

    updates

--- deploy-kops-cluster.sh
+++ deploy-kops-cluster.sh
@@ -9,13 +9,17 @@ apt-get -y install wget
 
 
 LAB_KOPS_AWS_KEY_ID=$(vault kv get -field=AccessKeyId concourse/cisco-fso-labs/us-east-2b/lab-kops)
+echo $LAB_KOPS_AWS_KEY_ID
 LAB_KOPS_AWS_KEY=$(vault kv get -field=SecretAccessKey concourse/cisco-fso-labs/us-east-2b/lab-kops)
+echo $LAB_KOPS_AWS_KEY
 
 aws configure set aws_access_key_id $LAB_KOPS_AWS_KEY_ID
 aws configure set aws_secret_access_key $LAB_KOPS_AWS_KEY
 aws configure set default.region us-east-2
 
-aws s3api create-bucket --bucket lab-kube.k8s.local --region --region us-east-2
+aws sts get-caller-identity --query Account --output text
+
+aws s3api create-bucket --bucket lab-kube.k8s.local --region us-east-2
 aws s3api put-bucket-versioning --bucket lab-kube.k8s.local --versioning-configuration Status=Enabled
 aws s3api create-bucket --bucket lab-kops -oidc-store --region us-east-2 --acl public-read
 
