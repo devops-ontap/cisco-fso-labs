9d45c8c95afa5c7f58795320435cab68d627d6c9
sconrod-tester <sconrod1@gmail.com>
2022-04-06 07:09:26 -0600

    updated hosts

--- deploy-iam-aws.sh
+++ deploy-iam-aws.sh
@@ -12,11 +12,18 @@ aws iam create-user --user-name lab-kops-test
 aws iam add-user-to-group --user-name lab-kops-test --group-name lab-kops-test
 aws iam create-access-key --user-name lab-kops-test
 
+#Here need the python to get the response from the AWS API in Python
+#Set the variables for the new AWS key and Secret and write to the vault
+#for now set this manually.
+#We need this in order to deploy the kube cluster via kops
+
 #Write the Key to the Vault
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $SSH_TOKEN
-#vault kv put concourse/main/lab-kops-test/AccessKeyId
-#vault kv put concourse/main/lab-kops-test/SecretAccessKey
+#vault kv put concourse/main/us-east-2b/lab-kops-test/AccessKeyId
+#vault kv put concourse/main/$NAME/lab-kops-test/AccessKeyId
+#vault kv put concourse/main/us-east-2b/lab-kops-test/SecretAccessKey
+#vault kv put concourse/main/$NAME/lab-kops-test/SecretAccessKey
 #Add the code here which will write the key and secret to the vault
 
