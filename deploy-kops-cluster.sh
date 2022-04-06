884aeeea14e56571db959c0e1f9af97df802a54a
sconrod-tester <sconrod1@gmail.com>
2022-04-06 12:29:31 -0600

    updates

--- deploy-kops-cluster.sh
+++ deploy-kops-cluster.sh
@@ -7,14 +7,6 @@ export AZ=$NAME
 export REGION=$REGION
 apt-get -y install wget
 
-kubectl --version
-
-#Install kops
-
-wget https://github.com/kubernetes/kops/releases/download/1.6.1/kops-linux-amd64
-chmod +x kops-linux-amd64
-mv kops-linux-amd64 /usr/local/bin/kops
-
 
 LAB_KOPS_AWS_KEY_ID=$(vault kv get -field=AccessKeyId concourse/cisco-fso-labs/us-east-2b/lab-kops)
 LAB_KOPS_AWS_KEY=$(vault kv get -field=SecretAccessKey concourse/cisco-fso-labs/us-east-2b/lab-kops)
@@ -27,7 +19,7 @@ aws s3api create-bucket --bucket lab-kube.k8s.local --region --region us-east-2
 aws s3api put-bucket-versioning --bucket lab-kube.k8s.local --versioning-configuration Status=Enabled
 aws s3api create-bucket --bucket lab-kops -oidc-store --region us-east-2 --acl public-read
 
-Create First Cluster:
+#Create First Cluster:
 export CLUSTER_NAME=lab-kube.k8s.local
 export KOPS_STATE_STORE=s3://lab-kube.k8s.local
 kops create cluster --zones=$NAME ${CLUSTER_NAME}
