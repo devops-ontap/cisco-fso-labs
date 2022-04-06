2b8937d69faff102335c077e82796548baa903e9
sconrod-tester <sconrod1@gmail.com>
2022-04-05 22:35:00 -0600

    updates

--- deploy-kube-cluster.sh
+++ deploy-kube-cluster.sh
@@ -6,3 +6,5 @@ mv ./kubectl /usr/local/bin/kubectl
 #Install kops
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
+kubectl --version
+kops --version
