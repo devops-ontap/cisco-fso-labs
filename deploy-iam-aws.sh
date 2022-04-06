99b7b1c94ca96fb2e1b8fd20505f43b531ed7351
sconrod-tester <sconrod1@gmail.com>
2022-04-06 10:55:26 -0600

    updates

--- deploy-iam-aws.sh
+++ deploy-iam-aws.sh
@@ -21,12 +21,26 @@ aws iam create-access-key --user-name lab-kops-test
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $SSH_TOKEN
-#vault kv put concourse/main/us-east-2b/lab-kops-test/AccessKeyId
+vault kv put concourse/main/us-east-2b/lab-kops-test/AccessKeyId
 #vault kv put concourse/main/$NAME/lab-kops-test/AccessKeyId
-#vault kv put concourse/main/us-east-2b/lab-kops-test/SecretAccessKey
+vault kv put concourse/main/us-east-2b/lab-kops-test/SecretAccessKey
 #vault kv put concourse/main/$NAME/lab-kops-test/SecretAccessKey
 #Add the code here which will write the key and secret to the vault
+LAB_KOPS_AWS_KEY_ID=$(vault kv get -field=token concourse/main/us-east-2b/lab-kops-test/AccessKeyId)
+LAB_KOPS_AWS_KEY=$(vault kv get -field=token concourse/main/us-east-2b/lab-kops-test/SecretAccessKey)
 
+aws configure set aws_access_key_id $LAB_KOPS_AWS_KEY_ID
+aws configure set aws_secret_access_key $LAB_KOPS_AWS_KEY
+aws configure set default.region us-east-2
 
+aws s3api create-bucket --bucket lab-kube.k8s.local --region --region us-east-2
+aws s3api put-bucket-versioning --bucket lab-kube.k8s.local --versioning-configuration Status=Enabled
+aws s3api create-bucket --bucket lab-kops -oidc-store --region us-east-2 --acl public-read
+
+Create First Cluster:
+export CLUSTER_NAME=lab-kube.k8s.local
+export KOPS_STATE_STORE=s3://lab-kube.k8s.local
+kops create cluster --zones=$NAME ${CLUSTER_NAME}
+kops update cluster ${CLUSTER_NAME} --yes
 
 
