02967f541a5de875ddcb5d58526d51b321743859
sconrod-tester <sconrod1@gmail.com>
2022-04-06 08:10:35 -0600

    updates

--- deploy-kube-cluster.sh
+++ deploy-kube-cluster.sh
@@ -1,4 +1,7 @@
 #Install kubectl
-RUN apk add --no-cache bash
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apk-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apk update && sudo apt-get install vault
+aws configure
 kubectl --version
 kops --version
