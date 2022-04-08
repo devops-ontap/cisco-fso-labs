381109002384c36f87041d2a659fceb067658d85
sconrod-tester <sconrod1@gmail.com>
2022-04-07 20:25:40 -0600

    updates

--- lab_vars.py
+++ lab_vars.py
@@ -1,5 +1,5 @@
-name='us-east-2c'
-region='us-east-2'
-az='us-east-2c'
+name='us-east-1b'
+region='us-east-1'
+az='us-east-1b'
 csr_ami_id='ami-00811671609253811'
 ubuntu_ami_id='ami-05117a5348bb8a291'
