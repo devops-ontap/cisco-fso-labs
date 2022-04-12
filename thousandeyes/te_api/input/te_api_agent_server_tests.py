00b27ee2eb6a6c010d107122c9334c554c2cfaf0
sconrod-tester <sconrod1@gmail.com>
2022-04-12 14:06:05 -0600

    updates

--- thousandeyes/te_api/input/te_api_agent_server_tests.py
+++ thousandeyes/te_api/input/te_api_agent_server_tests.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 import json, re, sys, os, json, subprocess, time, logging, requests, paramiko, urllib3
-import pandas as pd
 from subprocess import call, check_output
 from requests.structures import CaseInsensitiveDict
 urllib3.disable_warnings()
 token = os.getenv('TE_OATHTOKEN')
-#token = '1d0acd78-a470-44ad-a6d6-0892ac2db441'
-test_name = 'test-33'
+test_name = 'test-102'
 url = "https://api.thousandeyes.com/v6/agents.json"
 payload={}
 headers = {'Authorization': 'Bearer ' + token}
@@ -33,7 +31,7 @@ print(empty_list)
 #The expected format of the list of agents, is cumbersome because it requires you to create a list of dictionaries instead of just using the list of agentIDs
 #agents = [{'agentId': '443526'}, {'agentId': '443531'}]
 url='https://api.thousandeyes.com/v6/tests/agent-to-server/new.json'
-payload = {'interval': '300', 'agents': empty_list, 'testName': 'test-60', 'port': '80', 'server': 'www.thousandeyes.com','alertsEnabled': '0'}
+payload = {'interval': '300', 'agents': empty_list, 'testName': test_name, 'port': '80', 'server': 'www.thousandeyes.com','alertsEnabled': '0'}
 header = {'content-type': 'application/json', 'authorization': 'Bearer ' + token}
 r = requests.post(url, data=json.dumps(payload), headers=header, verify=False)
 print(r)
