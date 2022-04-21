f8568160680917a65b7a9df0540efba208574856
sconrod-tester <sconrod1@gmail.com>
2022-04-21 15:35:54 -0600

    updates

--- lab-tasks/appdynamics/appd-api/input/appd_api.sh
+++ lab-tasks/appdynamics/appd-api/input/appd_api.sh
@@ -6,5 +6,6 @@ rm -rf __pycache__
 pip3 install paramiko
 python3 get_token.py
 export SSH_TOKEN=$SSH_TOKEN
-vault login --no-print $SSH_TOKEN
+#vault login --no-print $SSH_TOKEN
+vault login $SSH_TOKEN
 vault kv put concourse/cisco-fso-labs/appd-oath key=token
