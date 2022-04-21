a7b7d8f750d3bd5e7a1593e0872979ef43ccbe3a
sconrod-tester <sconrod1@gmail.com>
2022-04-21 16:06:43 -0600

    updates

--- get_appd_token.py
+++ get_appd_token.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
-import json, re, sys, os, json, subprocess, time, logging, requests, urllib3
+import json, re, sys, os, json, subprocess, time, logging, requests, paramiko, urllib3
 from subprocess import call, check_output
 from requests.structures import CaseInsensitiveDict
 urllib3.disable_warnings()
 import requests
 
-#The AppD secret is passed in via Env Var which is injected into the build container via the pipeline
+
 #import the env vars and logon to vault to get the secret and then use it to run this command and write the output to the vault
-appd_secret = os.getenv('APPD_SECRET')
-#APPD_SECRET='cdb74fe0-a19e-4432-9350-9bb6ebc1fa56'
+token = os.getenv('APPD_SECRET')
+APPD_SECRET='cdb74fe0-a19e-4432-9350-9bb6ebc1fa56'
 
 url = "https://cisco-apipartnertraininglab.saas.appdynamics.com/auth/v1/oauth/token"
-payload='grant_type=client_credentials&client_id=fsolab4%40cisco-apipartnertraininglab&client_secret=' + str(appd_secret)
+payload='grant_type=client_credentials&client_id=fsolab4%40cisco-apipartnertraininglab&client_secret=' + APPD_SECRET
 print(payload)
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
+
+#print(response.text)
 print(response.json)
+
+#write the access token to the vault
+
+
 #put the bearer token into a var
 token_json = response.json()
 appd_token = token_json['access_token']
