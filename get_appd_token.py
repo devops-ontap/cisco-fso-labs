7da5af102c6c06590f6aa8aadcc6427d2d0e8857
sconrod-tester <sconrod1@gmail.com>
2022-04-21 15:56:38 -0600

    updates

--- get_appd_token.py
+++ get_appd_token.py
@@ -11,7 +11,7 @@ appd_secret = os.getenv('APPD_SECRET')
 #APPD_SECRET='cdb74fe0-a19e-4432-9350-9bb6ebc1fa56'
 
 url = "https://cisco-apipartnertraininglab.saas.appdynamics.com/auth/v1/oauth/token"
-payload='grant_type=client_credentials&client_id=fsolab4%40cisco-apipartnertraininglab&client_secret=' + appd_secret
+payload='grant_type=client_credentials&client_id=fsolab4%40cisco-apipartnertraininglab&client_secret=' + str(appd_secret)
 print(payload)
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -21,10 +21,10 @@ response = requests.request("POST", url, headers=headers, data=payload)
 
 #put the bearer token into a var
 token_json = response.json()
-APPD_TOKEN = token_json['access_token']
-print(APPD_TOKEN)
+appd_token = token_json['access_token']
+print(appd_token)
 
-os.environ["APPD_TOKEN"] = APPD_TOKEN
+os.environ["APPD_TOKEN"] = appd_token
 
 #write the access token to the vault (remember need to update this later so it does it for each environment - so each branch needs to have a user in vault with that name
 #Do this from the shell script until you add in the vault module for python here.....
