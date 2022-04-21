3de3e101ce150ada1615b8b55de46baa142f028e
sconrod-tester <sconrod1@gmail.com>
2022-04-21 15:37:42 -0600

    updates

--- get_appd_token.sh
+++ get_appd_token.sh
@@ -4,7 +4,7 @@ export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 #logging into vault with the lab account token to get the appd-secret and put to env var for python
 vault login --no-print $SSH_TOKEN
-APPD_SECRET=$(kv get --field=key concourse/cisco-fso-labs/appd-secret)
+APPD_SECRET=$(vault kv get --field=key concourse/cisco-fso-labs/appd-secret)
 export APPD_SECRET=$APPD_SECRET
 echo $APPD_SECRET
 #python can now connect to appd to generate the bearer token and set to var
