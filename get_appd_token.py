92f274a7b5e522041dc8664b1676df5c033bbdb6
sconrod-tester <sconrod1@gmail.com>
2022-04-21 16:24:45 -0600

    updates

--- get_appd_token.py
+++ get_appd_token.py
@@ -3,15 +3,13 @@ import json, re, sys, os, json, subprocess, time, logging, requests, urllib3
 from subprocess import call, check_output
 from requests.structures import CaseInsensitiveDict
 urllib3.disable_warnings()
-import requests
-
 
 #import the env vars and logon to vault to get the secret and then use it to run this command and write the output to the vault
-token = os.getenv('APPD_SECRET')
-APPD_SECRET='cdb74fe0-a19e-4432-9350-9bb6ebc1fa56'
+APPD_SECRET = os.getenv('APPD_SECRET')
+#APPD_SECRET='cdb74fe0-a19e-4432-9350-9bb6ebc1fa56'
 
 url = "https://cisco-apipartnertraininglab.saas.appdynamics.com/auth/v1/oauth/token"
-payload='grant_type=client_credentials&client_id=fsolab4%40cisco-apipartnertraininglab&client_secret=' + APPD_SECRET
+payload='grant_type=client_credentials&client_id=fsolab4%40cisco-apipartnertraininglab&client_secret=' + str(APPD_SECRET)
 print(payload)
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -19,7 +17,7 @@ headers = {
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
-#print(response.text)
+print(response.text)
 print(response.json)
 
 #write the access token to the vault
@@ -35,3 +33,12 @@ os.environ["APPD_TOKEN"] = appd_token
 #write the access token to the vault (remember need to update this later so it does it for each environment - so each branch needs to have a user in vault with that name
 #Do this from the shell script until you add in the vault module for python here.....
 
+#run the vault command using subprocess...
+
+cmd_vault_put='vault kv put concourse/cisco-fso-labs/appd-oath key=' + appd_token
+output = check_output("{}".format(cmd_vault_put), shell=True).decode().strip()
+print("Output: \n{}\n".format(output))
+
+cmd_vault_get='vault kv put concourse/cisco-fso-labs/appd-oath key=' + appd_token
+output = check_output("{}".format(cmd_vault_get), shell=True).decode().strip()
+print("Output: \n{}\n".format(output))
