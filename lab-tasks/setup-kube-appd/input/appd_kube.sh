1fb2b385a6227ec3dd71316f8cf0c7f9c8ba0ab5
sconrod-tester <sconrod1@gmail.com>
2022-04-28 22:00:10 -0600

    updates

---
+++ lab-tasks/setup-kube-appd/input/appd_kube.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+export AWS_PAGER=""
+cp config ~/.aws
+export VAULT_ADDR=$VAULT_ADDR
+export VAULT_TOKEN=$SSH_TOKEN
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x ./kops
+mv ./kops /usr/local/bin/
+export KOPS_STATE_STORE=s3://lab-kube.k8s.local
+kops export kubecfg --admin
+wget https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml -O metrics-server-components.yaml
+kubectl apply -f metrics-server-components.yaml
+kubectl get deployment metrics-server -n kube-system
+kubectl create ns appdynamics
+helm repo add appdynamics-charts https://ciscodevnet.github.io/appdynamics-charts
+#The put to vault needs to be done once, do not include yaml in this dir!
+#vault kv put concourse/cisco-fso-labs/appd-helm-values data=$(base64 < values.yaml)
+vault kv get -field data concourse/cisco-fso-labs/appd-helm-values | base64 -d > values.yaml
+
+
+
+#helm install -f ./values.yaml appd-cluster-agent appdynamics-charts/cluster-agent --namespace=appdynamics
+
