3ed23ba52aa3e782af87c0e6795ca0cbb24e2043
sconrod-devops <sconrod1@gmail.com>
2022-05-11 08:49:41 -0700

    Deprovision (#26)
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * moved create key_pair to step 1 ahead of create_vpc
    
    * moved create key_pair to step 1 ahead of create_vpc
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates to deprov
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates

---
+++ lab-tasks/deprovision/input/deprov_ec2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import json, re, sys, os, json, time, logging, requests, urllib3
+from requests.structures import CaseInsensitiveDict
+urllib3.disable_warnings()
+from requests.structures import CaseInsensitiveDict
+import subprocess
+from subprocess import call, check_output
+
+VAULT_ADDR = os.getenv('VAULT_ADDRR')
+VAULT_TOKEN = os.getenv('VAULT_TOKEN') #This gets the vault token from the ephemeral build container
+
+lab_vars='lab_vars.py'
+import lab_vars
+from lab_vars import *
+
+#get all the vars and then import this module to a delete_env script that uses the aws-cli commands and polls for inst state when termed
+
+VAULT_ADDR = os.getenv('VAULT_ADDRR')
+VAULT_TOKEN = os.getenv('VAULT_TOKEN')
+
+
+name='us-east-2b'
+#Get List of all EC2 instances in a AZ
+#aws ec2 describe-instances --query "Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key=='us-east-2c']|[0].Value}" --output json
+cmd_describe_instances='aws ec2 describe-instances --query "Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==' + "'" "{}".format(name) + "'" + ']|[0].Value}"' + " " + '--output json'
+output = check_output("{}".format(cmd_describe_instances), shell=True).decode().strip()
+y=json.loads(output)
+list_json = y
+list_of_lists=list_json
+flatList = [ item for elem in list_of_lists for item in elem]
+res = [ sub['Instance'] for sub in flatList ]
+print(str(res))
