54891a6511b396db6d621fee0b17c451c7bf213f
sconrod-tester <sconrod1@gmail.com>
2022-05-11 18:19:13 -0600

    updates

--- lab-tasks/deprovision/input/deprov.py
+++ lab-tasks/deprovision/input/deprov.py
@@ -17,7 +17,7 @@ from lab_vars import *
 
 VAULT_ADDR = os.getenv('VAULT_ADDRR')
 VAULT_TOKEN = os.getenv('VAULT_TOKEN')
-url = "http://vault.devops-ontap.com:8200/v1/concourse/cisco-fso-labs/" + name + "/" + "key_name"
+url = "http://vault.devops-ontap.com:8200/v1/concourse/cisco-fso-labs/" + name + "/" + "ssh-key"
 headers = CaseInsensitiveDict()
 headers["X-Vault-Token"] = VAULT_TOKEN
 headers["Content-Type"] = "application/json"
@@ -26,8 +26,9 @@ resp = requests.request("GET", url, headers=headers, json=data_json)
 print(resp.json)
 print(resp.status_code)
 key_name_json = resp.json()
-key_name_dict = key_name_json['data']
-key_name = key_name_dict['key_name']
+print(key_name_json)
+key_name_dict = key_name_json['Key']
+key_name = key_name_dict['ssh-key']
 
 
 
@@ -122,10 +123,25 @@ print(resp.json)
 print(resp.status_code)
 rt_rt_id_json = resp.json()
 rt_rt_id_dict = rt_rt_id_json['data']
-rt_rt_id = rt_rt_id_dict['rt_rt_id']
+rt_rt_id = rt_lan_id_dict['rt_rt_id']
 print(rt_rt_id)
 
-#aws ec2 delete-route-table --route-table-id $rt_rt_id
+
+#11 - Read rt_lan_id from vault
+url = "http://vault.devops-ontap.com:8200/v1/concourse/cisco-fso-labs/" + name + "/" + "rt_lan_id"
+headers = CaseInsensitiveDict()
+headers["X-Vault-Token"] = VAULT_TOKEN
+headers["Content-Type"] = "application/json"
+data_json = {}
+resp = requests.request("GET", url, headers=headers, json=data_json)
+print(resp.json)
+print(resp.status_code)
+rt_lan_id_json = resp.json()
+rt_lan_id_dict = rt_lan_id_json['data']
+rt_lan_id = rt_lan_id_dict['rt_lan_id']
+print(rt_lan_id)
+
+
 
 #13 - Read the router_sg_id from vault
 
@@ -146,25 +162,26 @@ print(router_sg_id)
 #AWS should be authenticated in the build container via the task args..
 
 
+'''
 
 
 #Delete the Route Tables
 
-#aws ec2 delete-route-table --route-table-id rt_lan_id
-cmd_del_rt_rt_id='aws ec2 delete-route-table --route-table-id + "{}".format(rt_rt_id)
+#aws ec2 delete-route-table --route-table-id rt_rt_id
+cmd_del_rt_rt_id='aws ec2 delete-route-table --route-table-id' + " " + "{}".format(rt_rt_id)
 print(cmd_del_rt_rt_id)
 output = check_output("{}".format(cmd_del_rt_rt_id), shell=True).decode().strip()
 print("Output: \n{}\n".format(output))
 
 
-#aws ec2 delete-route-table --route-table-id rt_rt_id
-cmd_del_rt_lan_id='aws ec2 delete-route-table --route-table-id + "{}".format(rt_lan_id)
+#aws ec2 delete-route-table --route-table-id rt_lan_id
+cmd_del_rt_lan_id='aws ec2 delete-route-table --route-table-id' + "{}".format(rt_lan_id)
 print(cmd_del_rt_lan_id)
 output = check_output("{}".format(cmd_del_rt_lan_id), shell=True).decode().strip()
 print("Output: \n{}\n".format(output))
 
 #aws ec2 delete-internet-gateway --internet-gateway-id igid
-cmd_del_igid='aws ec2 delete-internet-gateway --internet-gateway-id + "{}".format(igid)
+cmd_del_igid='aws ec2 delete-internet-gateway --internet-gateway-id' + " "  + "{}".format(igid)
 print(cmd_del_igid)
 output = check_output("{}".format(cmd_del_igid), shell=True).decode().strip()
 print("Output: \n{}\n".format(output))
@@ -172,13 +189,13 @@ print("Output: \n{}\n".format(output))
 #Delete the subnets
 
 #aws ec2 delete-subnet
-cmd_del_router_sg_id='aws ec2 delete-subnet --subnet-id + "{}".format(subnetid_router)
+cmd_del_router_sg_id='aws ec2 delete-subnet --subnet-id' + " " + "{}".format(subnetid_router)
 print(cmd_subnetid_router)
 output = check_output("{}".format(cmd_rsubnetid_router), shell=True).decode().strip()
 print("Output: \n{}\n".format(output))
 
 #aws ec2 delete-subnet
-cmd_del_subnetid_lan='aws ec2 delete-subnet --subnet-id + "{}".format(subnetid_lan)
+cmd_del_subnetid_lan='aws ec2 delete-subnet --subnet-id' " " + "{}".format(subnetid_lan)
 print(cmd_subnetid_lan)
 output = check_output("{}".format(cmd_subnetid_lan), shell=True).decode().strip()
 print("Output: \n{}\n".format(output))
@@ -186,23 +203,27 @@ print("Output: \n{}\n".format(output))
 
 #Delete the Internet Gateway
 #aws ec2 delete-internet-gateway --internet-gateway-id igid
-cmd_del_igid='aws ec2 delete-internet-gateway --internet-gateway-id + "{}".format(igid)
-print(cmd_igid)
-output = check_output("{}".format(cmd_igid)
+cmd_del_igid='aws ec2 delete-internet-gateway --internet-gateway-id' " " + "{}".format(igid)
+output = check_output("{}".format(cmd_del_igid), shell=True).decode().strip()
+print("Output: \n{}\n".format(output))
+
 
 #Delete the Keys
 #aws ec2 delete-key-pair --key-name
-cmd_del_vpcid='aws ec2 delete-key-pair --key-name + "{}".format(key_name)
-print(cmd_vpcid)
-output = check_output("{}".format(cmd_vpcid)
-
-Delete the VPC
-aws ec2 delete-vpc --vpc-id vpcid
-cmd_del_key_name='aws ec2 delete-vpc --vpcid-id + "{}".format(vpcid)
-print(cmd_key_name)
-output = check_output("{}".format(cmd_key_name)
+cmd_del_keys='aws ec2 delete-key-pair --key-name' + " " + "{}".format(key_name)
+print(cmd_del_keys)
+output = check_output("{}".format(cmd_del_keys), shell=True).decode().strip()
+print("Output: \n{}\n".format(output))
 
 
+#Delete the VPC
+cmd_del_vpc='aws ec2 delete-vpc --vpc-id' + " " + "{}".format(vpcid)
+print(cmd_del_vpc)
+output = check_output("{}".format(cmd_del_vpc), shell=True).decode().strip()
+print("Output: \n{}\n".format(output))
+
+
+'''
 
 
 
