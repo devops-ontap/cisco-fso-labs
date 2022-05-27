a891d40572c7ac478b5cc9e0ab005ac3fc86bd47
sconrod-tester <sconrod1@gmail.com>
2022-05-27 11:41:45 -0600

    updates

--- lab-tasks/intersight/intersight_api_auth/input/intersight-auth.sh
+++ lab-tasks/intersight/intersight_api_auth/input/intersight-auth.sh
@@ -7,6 +7,6 @@ pip3 install cryptography
 pip3 install git+https://github.com/CiscoDevNet/intersight-python
 keyid=$(vault kv get --field=keyid concourse/cisco-fso-labs/intersight/keyid_v2)
 export keyid=$keyid
-secret=$(vault kv get --field=secret concourse/cisco-fso-labs/intersight/secret_git v2)
+vault kv get --field=secret concourse/cisco-fso-labs/intersight/secret_v2 > SecretKey.txt
 export secret=$secret
-python3 GetComputeRackUnits.py
+python3 PhysicalSummaries_v2.py
