8494cefbf88df4655ddb16c562212c1152f00dd5
sconrod-tester <sconrod1@gmail.com>
2022-06-08 17:48:39 -0600

    updates

--- lab-kube/setup_supercar_trader_app/input/supercar_trader.sh
+++ lab-kube/setup_supercar_trader_app/input/supercar_trader.sh
@@ -10,6 +10,7 @@ mkdir ~/.kube
 vault kv get -field kubeconfig concourse/cisco-fso-labs/lab-kube-config > ~/.kube/config
 kubectl create ns supercar-trader
 #vault kv get -field data concourse/cisco-fso-labs/supercar-trader-values | base64 -d > values.yaml
+kubectl -n supercar-trader delete deploy
 kubectl -n supercar-trader apply -f supercar-trader.yml
 kubectl -n supercar-trader apply -f tomcat_lb.yml
 kubectl -n supercar-trader get svc -w
