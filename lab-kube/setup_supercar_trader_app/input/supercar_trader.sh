581ccd1c3422e16b07d283f504b9e62955382dae
sconrod-tester <sconrod1@gmail.com>
2022-06-08 18:00:30 -0600

    updates

--- lab-kube/setup_supercar_trader_app/input/supercar_trader.sh
+++ lab-kube/setup_supercar_trader_app/input/supercar_trader.sh
@@ -3,6 +3,9 @@ export AWS_PAGER=""
 cp config ~/.aws
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x ./kops
+mv ./kops /usr/local/bin/
 export KOPS_STATE_STORE=s3://lab-kube.k8s.local
 kops export kubecfg --admin
 vault login --no-print $SSH_TOKEN
