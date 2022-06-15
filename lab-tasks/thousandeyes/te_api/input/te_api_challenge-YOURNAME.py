bf4b661f3ba2724dacb08fa1b9964ab176d575ba
sconrod-tester <sconrod1@gmail.com>
2022-06-15 17:47:48 -0600

    updates

--- lab-tasks/thousandeyes/te_api/input/te_api_challenge-YOURNAME.py
+++ lab-tasks/thousandeyes/te_api/input/te_api_challenge-YOURNAME.py
@@ -4,7 +4,8 @@ from subprocess import call, check_output
 from requests.structures import CaseInsensitiveDict
 urllib3.disable_warnings()
 token = os.getenv('TE_OATHTOKEN')
-url = "https://api.thousandeyes.com/v6/status.json"
+print(token)
+url = "https://api.thousandeyes.com/v6/tests/agent-to-server.json"
 payload={}
 headers = {'Authorization': 'Bearer ' + token}
 response = requests.request("GET", url, headers=headers, data=payload)
