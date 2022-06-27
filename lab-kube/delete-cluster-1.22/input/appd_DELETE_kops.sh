0fc2f7691da1a00563521e5ba1892218131be8e4
sconrod-tester <sconrod1@gmail.com>
2022-06-27 17:18:35 -0600

    updates

--- lab-kube/delete-cluster-1.22/input/appd_DELETE_kops.sh
+++ lab-kube/delete-cluster-1.22/input/appd_DELETE_kops.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export AWS_PAGER=""
-cp config ~/.aws
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
