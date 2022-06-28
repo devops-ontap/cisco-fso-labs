e6790a0a5b4af4162cfd4b0beccc5bb92ec43608
sconrod-tester <sconrod1@gmail.com>
2022-06-28 09:54:45 -0600

    updates

--- lab-kube/create-cluster-1.22/input/appd_kops-1.22.sh
+++ lab-kube/create-cluster-1.22/input/appd_kops-1.22.sh
@@ -6,8 +6,9 @@ curl -LO https://github.com/kubernetes/kops/releases/download/v1.22.0/kops-linux
 cp kops-linux-amd64 kops
 chmod +x ./kops
 mv ./kops /usr/local/bin/
-export NAME=lab-kube.k8s.local
+export NAME=lab-2-kube.k8s.local
 export KOPS_STATE_STORE=s3://lab-kube.k8s.local
+#The s3 bucket needs to be created - this was done manually - require to automate into pipeline
 kops create cluster --zones=us-east-2a ${NAME}
 kops update cluster ${NAME} --yes
 export KOPS_STATE_STORE=s3://lab-kube.k8s.local
@@ -15,7 +16,8 @@ kops export kubecfg --admin
 #Need to get the kubeconfig file to yaml and then write it to the vault so in the subsequent tasks it can be called from the vault.....
 vault login --no-print $SSH_TOKEN
 cp ~/.kube/config .
-vault kv put concourse/cisco-fso-labs/lab-kube-config kubeconfig=@config
+vault kv put concourse/cisco-fso-labs/lab-2-kube-config kubeconfig=@config
+kops validate cluster --wait 10m
 
 
 
--- lab-kube/setup_appd_kube_cluster_agent/input/appd_helm.sh
+++ lab-kube/setup_appd_kube_cluster_agent/input/appd_helm.sh
@@ -16,8 +16,8 @@ mkdir ~/.kube
 vault kv get -field kubeconfig concourse/cisco-fso-labs/lab-kube-config > ~/.kube/config
 kubectl create ns appdynamics-lab-2
 vault kv get -field data concourse/cisco-fso-labs/appd-helm-values | base64 -d > values.yaml
-helm install -f ./values.yaml appdynamics-lab appdynamics-charts/cluster-agent -n appdynamics-lab-2
-helm status appdynamics-lab-2 -n default
-helm get appdynamics-lab-2 -n default
-kubectl get pods -n appdynamics-lab-2
+helm install -f ./values.yaml appdynamics-lab appdynamics-charts/cluster-agent -n appdynamics-lab
+helm status appdynamics-lab -n default
+helm get appdynamics-lab -n default
+kubectl get pods -n appdynamics-lab
 
